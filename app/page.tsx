94675bc9463601ce8ad87d4acc2c5600b8009b08
hdans <danishrme@gmail.com>
2025-10-24 01:35:53 +0700

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ const CustomCursor = () => {
   const cursorPositionRef = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
-    const updateMousePosition = (e) => {
+     const updateMousePosition = (e: MouseEvent) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
 
